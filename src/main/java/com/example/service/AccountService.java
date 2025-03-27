21df89722db3e43b70a5541b7d247356ad7ff128
Darshil Patel <darshilpatel2020@gmail.com>
2025-03-27 17:22:28 +0000

    Updated Project

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -7,6 +7,7 @@ import com.example.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class AccountService {
     public enum RegisterStatus {
